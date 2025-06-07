0f9bbd41dd5c50fb84dcfcae516e601d41d3d8f4
GMarcolla <gustavomarcolla123@gmail.com>
2025-06-07 11:54:51 -0300

    Criação do controlador das categorias

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+033cfdec9cd0c1efe9aa2fa7f2b2855773723d5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ce3db688a56004023b76425a1a0c4953268a75")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
