0637b52c877f7635a3d13a5bd42a43470ee9016c
GMarcolla <gustavomarcolla123@gmail.com>
2025-06-07 12:10:21 -0300

    Configura a classe para não obrigar a preencher as informações da categoria

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ce3db688a56004023b76425a1a0c4953268a75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f9bbd41dd5c50fb84dcfcae516e601d41d3d8f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
