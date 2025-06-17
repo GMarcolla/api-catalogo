a9a029c9f50a61405753526e83ecfb4b3d8c4254
Gustavo.Marcolla <gustavo.marcolla@senior.com.br>
2025-06-17 13:21:06 -0300

    Adiciona Validators customizados

---
+++ APICatalogo/Validation/PrimeiraLetraMaiusculaAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.Validation
+{
+    public class PrimeiraLetraMaiusculaAttribute : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is string strValue && !string.IsNullOrEmpty(strValue))
+            {
+                if (char.IsUpper(strValue[0]))
+                {
+                    return ValidationResult.Success;
+                }
+                else
+                {
+                    return new ValidationResult("A primeira letra deve ser maiúscula.");
+                }
+            }
+            return new ValidationResult("O valor não pode ser nulo ou vazio.");
+        }
+    }
+}
